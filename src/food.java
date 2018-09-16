df5b0d1acb8e539695cb50036ea9d0d7b852c719
JasonSaber <jsaber19@windwardschool.org>
2018-09-16 15:55:56 -0700

    initial

---
+++ src/food.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Field;
+
+public class food {
+    private boolean sweet, salty, bitter, savory, sour;
+    private int calories;
+    private String name;
+
+    // constructor
+    public food(boolean sweet, boolean salty, boolean bitter, boolean savory, boolean sour, int calories, String name){
+        this.sweet = sweet;
+        this.salty = salty;
+        this.bitter = bitter;
+        this.savory = savory;
+        this.sour = sour;
+        this.calories = calories;
+        this.name = name;
+    }
+
+    // makes it sweet
+    public void addSugar(){
+        sweet = true;
+    }
+
+    // makes it salty
+    public void addSalt(){ // makes it salty
+        salty = true;
+    }
+
+    // describes the food using the fields
+    public void describeMe(){
+        System.out.print(name + " has " + calories + " calories. ");
+        for (Field f : this.getClass().getFields()){ // really roundabout way of making sure I'm able to access the name of the variables because idk how else
+            if (f.equals(true)) System.out.print(name + " is " + f.getName() + ". ");
+        }
+    }
+
+}
